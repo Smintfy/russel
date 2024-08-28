488880d30a2d38c7ec591234e6cadce9e7ea6211
sveltie <lucydabestt@gmail.com>
2024-08-28 15:00:59 +0800

    More stuffs but it's messy :3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,66 @@
 use std::io::{self, Write};
-use std::process;
+use std::{env, process};
 
-fn main() {
+fn execute(tokens: Vec<&str>) {
+    // Split the tokens into [command] *[arg]
+    let cmd = tokens[0];
+    let argv = &tokens[1..];
+
+    let builtin_cmds = ["exit", "echo", "type"];
+
+    if builtin_cmds.contains(&cmd) {
+        match cmd {
+            "exit" => {
+                match argv.len() {
+                    0 => process::exit(0),
+                    1 => {
+                        let code = argv[0]
+                                    .parse::<i32>()
+                                    .expect("Error: Expect exit code to be type of i32");
+                        
+                        println!("Exited with code {}", code);
+                        process::exit(code);
+                    },
+                    _ => println!("Error: {} takes 1 argument but got {}", cmd, argv.len())
+                }
+            },
+            "echo" => println!("{}", argv.join(" ")),
+            "type" => {
+                for arg in argv {
+                    if builtin_cmds.contains(arg) {
+                        println!("{} is a shell builtin", arg);
+                    } else {
+                        if let Some(env_path) = env::var("PATH")
+                                    .unwrap()
+                                    .split(':')
+                                    .map(|path| format!("{}/{}", path, arg))
+                                    .find(|path| std::fs::metadata(path).is_ok())
+                        {
+                            println!("{} is {}", arg, env_path)
+                        } else {
+                            println!("type: {}: not found", arg);
+                        }
+                        
+                    }
+                }
+            }
+            _ => unreachable!()
+        }
+    } else if let Some(program) = env::var("PATH")
+                                    .unwrap()
+                                    .split(':')
+                                    .map(|path| format!("{}/{}", path, cmd))
+                                    .find(|path| std::fs::metadata(path).is_ok())
+    {
+        process::Command::new(program)
+            .status()
+            .expect("Error: failed to execute program");
+    } else {
+        println!("Command {} not found", cmd);
+    }
+}
+
+fn run() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -15,17 +74,11 @@ fn main() {
             continue;
         }
 
-        let token: Vec<&str> = command.split_whitespace().collect();
-
-        match token[..] {
-            ["exit"] => process::exit(0),
-            ["exit", code] => {
-                println!("Exit with code {}", code);
-                process::exit(code.parse::<i32>()
-                                        .expect("Error: Expect exit code to be type of i32"));
-            },
-            ["echo", ..] => println!("{}", token[1..].join(" ")),
-            _ => println!("Command {} not found", command),
-        };
+        let tokens: Vec<&str> = command.split_whitespace().collect();
+        execute(tokens);
     }
+}
+
+fn main() {
+    run();
 }
