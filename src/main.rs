ca76c47fb4b91c685fb44c1fbe1d01b52f4a33e4
sveltie <lucydabestt@gmail.com>
2024-09-01 11:59:12 +0800

    Add pwd and cd command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::io::{self, Write};
 use std::{env, process};
+use std::path::{Path, PathBuf};
 
-const BUILTIN_CMDS: [&str; 3] = ["exit", "echo", "type"];
+// TODO: Use enum instead.
+const BUILTIN_CMDS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
 
 fn main() {
     run();
 }
 
 fn run() {
+    const YELLOW: &str = "\x1b[33m";
+    const RESET: &str = "\x1b[0m";
+
+    let username = String::from_utf8(
+        process::Command::new("whoami").output().unwrap().stdout
+    ).unwrap();
+    let hostname = String::from_utf8(
+        process::Command::new("hostname").output().unwrap().stdout
+    ).unwrap();
+
     loop {
-        print!("$ ");
+        let cwd = get_cwd();
+        let simplified_cwd = cwd.to_str().unwrap().split('/').last().unwrap();
+
+        print!("{}{}@{}{}: {}$ ", YELLOW, username.trim(), hostname.trim(), RESET, simplified_cwd.replace("\"", ""));
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
@@ -32,6 +47,10 @@ fn find_cmd_path(cmd: &str) -> Option<String> {
         .find(|path| std::fs::metadata(path).is_ok())
 }
 
+fn get_cwd() -> PathBuf {
+    env::current_dir().unwrap()
+}
+
 fn execute(tokens: Vec<&str>) {
     let (cmd, args) = tokens.split_first().unwrap();
 
@@ -40,10 +59,15 @@ fn execute(tokens: Vec<&str>) {
             "exit" => execute_exit(args),
             "echo" => execute_echo(args),
             "type" => execute_type(args),
+            "pwd" => execute_pwdir(),
+            "cd" => execute_cd(args),
             _ => unreachable!()
         }
     } else if let Some(program) = find_cmd_path(cmd) {
-        process::Command::new(program).status().expect("Error: failed to execute program");
+        process::Command::new(program)
+            .args(args)
+            .status()
+            .expect("Error: failed to execute program");
     } else {
         println!("Command {} not found", cmd);
     }
@@ -74,5 +98,22 @@ fn execute_type(args: &[&str]) {
         } else {
             println!("type: {}: not found", arg);
         }
+    }
+}
+
+fn execute_pwdir() {
+    println!("{}", get_cwd().display());
+}
+
+fn execute_cd(args: &[&str]) {
+    let home = env::var("HOME").unwrap();
+    let mut target_dir = args[0].to_string();
+
+    if target_dir.starts_with('~') {
+        target_dir = target_dir.replacen("~", &home, 1);
+    }
+
+    if std::env::set_current_dir(Path::new(&target_dir)).is_err() {
+        println!("cd: {}: No such file or directory", args[0]);
     }
 }
