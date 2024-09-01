83c4e77683ef33999c70de7f5f5540347a3ee5ce
sveltie <lucydabestt@gmail.com>
2024-09-01 10:14:36 +0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,10 @@
 use std::io::{self, Write};
 use std::{env, process};
 
-fn execute(tokens: Vec<&str>) {
-    // Split the tokens into [command] *[arg]
-    let cmd = tokens[0];
-    let argv = &tokens[1..];
+const BUILTIN_CMDS: [&str; 3] = ["exit", "echo", "type"];
 
-    let builtin_cmds = ["exit", "echo", "type"];
-
-    if builtin_cmds.contains(&cmd) {
-        match cmd {
-            "exit" => {
-                match argv.len() {
-                    0 => process::exit(0),
-                    1 => {
-                        let code = argv[0]
-                                    .parse::<i32>()
-                                    .expect("Error: Expect exit code to be type of i32");
-                        
-                        println!("Exited with code {}", code);
-                        process::exit(code);
-                    },
-                    _ => println!("Error: {} takes 1 argument but got {}", cmd, argv.len())
-                }
-            },
-            "echo" => println!("{}", argv.join(" ")),
-            "type" => {
-                for arg in argv {
-                    if builtin_cmds.contains(arg) {
-                        println!("{} is a shell builtin", arg);
-                    } else {
-                        if let Some(env_path) = env::var("PATH")
-                                    .unwrap()
-                                    .split(':')
-                                    .map(|path| format!("{}/{}", path, arg))
-                                    .find(|path| std::fs::metadata(path).is_ok())
-                        {
-                            println!("{} is {}", arg, env_path)
-                        } else {
-                            println!("type: {}: not found", arg);
-                        }
-                        
-                    }
-                }
-            }
-            _ => unreachable!()
-        }
-    } else if let Some(program) = env::var("PATH")
-                                    .unwrap()
-                                    .split(':')
-                                    .map(|path| format!("{}/{}", path, cmd))
-                                    .find(|path| std::fs::metadata(path).is_ok())
-    {
-        process::Command::new(program)
-            .status()
-            .expect("Error: failed to execute program");
-    } else {
-        println!("Command {} not found", cmd);
-    }
+fn main() {
+    run();
 }
 
 fn run() {
@@ -66,19 +13,66 @@ fn run() {
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-
         io::stdin().read_line(&mut input).unwrap();
-        let command = input.trim();
 
-        if command.is_empty() {
+        let source = input.trim();
+        
+        if source.is_empty() {
             continue;
         }
 
-        let tokens: Vec<&str> = command.split_whitespace().collect();
-        execute(tokens);
+        let sequence: Vec<&str> = source.split_whitespace().collect();
+        execute(sequence);
     }
 }
 
-fn main() {
-    run();
+fn find_cmd_path(cmd: &str) -> Option<String> {
+    env::var("PATH").unwrap().split(':')
+        .map(|path| format!("{}/{}", path, cmd))
+        .find(|path| std::fs::metadata(path).is_ok())
+}
+
+fn execute(tokens: Vec<&str>) {
+    let (cmd, args) = tokens.split_first().unwrap();
+
+    if BUILTIN_CMDS.contains(cmd) {
+        match *cmd {
+            "exit" => execute_exit(args),
+            "echo" => execute_echo(args),
+            "type" => execute_type(args),
+            _ => unreachable!()
+        }
+    } else if let Some(program) = find_cmd_path(cmd) {
+        process::Command::new(program).status().expect("Error: failed to execute program");
+    } else {
+        println!("Command {} not found", cmd);
+    }
+}
+
+fn execute_exit(args: &[&str]) {
+    match args.len() {
+        0 => process::exit(0),
+        1 => {
+            let code = args[0].parse::<i32>().expect("Error: numeric argument required");
+            println!("Exited with code {:?}", code);
+            process::exit(code);
+        },
+        _ => println!("Error: exit takes 1 argument but got {}", args.len())
+    }
+}
+
+fn execute_echo(args: &[&str]) {
+    println!("{}", args.join(" "));
+}
+
+fn execute_type(args: &[&str]) {
+    for arg in args {
+        if BUILTIN_CMDS.contains(arg) {
+            println!("{} is a shell builtin", arg);
+        } else if let Some(env_path) = find_cmd_path(arg) {
+            println!("{} is {}", arg, env_path)
+        } else {
+            println!("type: {}: not found", arg);
+        }
+    }
 }
